33fdbeb729be53b380bb1be9694714342cf82bb2
winery <1255399340@qq.com>
2024-08-25 16:36:13 +0800

    gui自动化和查询

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 from wxauto import *
 import time
 import threading
@@ -8,27 +7,30 @@ import pygetwindow as gw
 from tkinter import *
 
 wx = WeChat()
-delay_seconds = 2
-
-def judgeChar(string, char):
+delay_seconds = 60
+def judgeChar(string,char):
     pattern = re.compile(char)
-    return bool(re.search(pattern, string))
+    if re.search(pattern,string):
+        return True
+    else:
+        return False
 
 def SelectContent(string):
-    string = string.strip().replace(" ", "")
+    string = string.strip().replace(" ","")
     pattern = r"{}(\w+)".format("查询")
-    matches = re.findall(pattern, string)
-    return matches[0] if matches else None
-
+    matches = re.findall(pattern,string)
+    for match in matches:
+        return match
 def auto_reply():
     try:
         msgs = wx.GetAllNewMessage()
+        psmes ="正在为您查询\n请稍后......\n如长时间未响应请重新查询"
         current = wx.CurrentChat()
         if msgs != {}:
             for key in msgs.keys():
                 if key == current:
                     message = str(msgs.get(key)[-1])
-                    if judgeChar(message, "查询"):
+                    if judgeChar(message,"查询"):
                         reContent = SelectContent(message)
                         overMessage = pickup_code(reContent)
                         dangdang = f"单号：{overMessage.get('单号')}\n状态：{overMessage.get('状态')}\n快递公司：{overMessage.get('快递公司')}\n取件码：{overMessage.get('取件码')}\n入库时间：{overMessage.get('入库时间')}\n温馨提示：为避免同学快递丢失请速速取走您的包裹，如有取错请及时联系工作人员。\n快递问题投诉：19178467032"
@@ -37,10 +39,11 @@ def auto_reply():
     except Exception as e:
         print("Error in auto_reply:", e)
 
+
 def New_friend():
     try:
         newfriend = wx.GetNewFriends()
-        if newfriend:
+        if newfriend != []:
             newfriend[0].Accept()
         else:
             wx.SwitchToChat()
@@ -48,10 +51,10 @@ def New_friend():
         print("Error in New_friend:", e)
 
 def menuClick():
-    moveClick(74, 409)
+    moveClick(74,409)
 
 def inputClick():
-    moveClick(514, 118)
+    moveClick(514,118)
 
 def inputclear():
     inputClick()
@@ -59,7 +62,7 @@ def inputclear():
     gui.press("BackSpace")
     time.sleep(0.5)
 
-def moveClick(x, y):
+def moveClick(x,y):
     gui.moveTo(x, y, duration=0.5)
     gui.click()
 
@@ -116,9 +119,9 @@ def clickSearch():
 def NotFound():
     try:
         gui.locateOnScreen("nf.png")
-        return "true"
+        return True
     except:
-        return "false"
+        return False
 
 def pickup_code(text):
     try:
@@ -128,31 +131,29 @@ def pickup_code(text):
         queryInput(text)
         clickSearch()
         time.sleep(0.5)
-        if NotFound() == "false":
+        if NotFound():
+            wx.SendMsg("未查到相关数据")
+        else:
             nnbb = context()
             return nnbb
-        else:
-            wx.SendMsg("未查到相关数据")
     except Exception as e:
         print("Error in pickup_code:", e)
 
 def context():
     status = ctlc('status').replace("\n", "")
     need = {
-        "单号": ctlc('num'),
-        "状态": status[:3],
-        "快递公司": ctlc('name'),
-        "取件码": ctlc('code'),
-        "入库时间": status[3:],
+        "单号":ctlc('num'),
+        "状态":status[:3],
+        "快递公司":ctlc('name'),
+        "取件码":ctlc('code'),
+        "入库时间":status[3:],
     }
     return need
 
 def main():
     while True:
         auto_reply()
-        thread = threading.Timer(delay_seconds, New_friend)
+        thread = threading.Timer(delay_seconds,New_friend())
         thread.start()
-        thread.join()  # 确保线程完成后再继续执行下一个循环
-
 if __name__ == "__main__":
-   main()
+    main()
